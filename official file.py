38491d6d0edb6f591824e51a9a44c59c09d4fd48
Ha Tran (Blake) <blaketrn@udel.edu>
2024-02-25 06:34:37 -0500

    updated until class stage

--- official file.py
+++ official file.py
@@ -8,12 +8,18 @@ from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 from html.entities import name2codepoint
 
+#import dataclass
+from dataclasses import dataclass
+
 # import json 
 import json 
+
 #import html
 import html
+
 #import pandas
 import pandas as pd
+
 #import time
 import time
 
@@ -31,105 +37,125 @@ data_json = str(json.loads(response.read()))
 # print the json response 
 # print(data_json) 
 
-# def parse_html(html):
-#     elem = BeautifulSoup(html, features="html.parser")
-#     text = ''
-#     for e in elem.descendants:
-#         if isinstance(e, str):
-#             text += e.get_text().strip()
-#         elif e.name in ['span']:
-#             text += ' '
-#         elif e.name in ['br',  'p', 'h1', 'h2', 'h3', 'h4', 'tr', 'th', 'div']:
-#             text += '\n'
-#         elif e.name == 'li':
-#             text += '\n- '
-#     return text
-
 code_body = []
 
 class MyHTMLParser(HTMLParser):
-    # def handle_starttag(self, tag, attrs):
-    #     print("Start tag:", tag)
-    #     for attr in attrs:
-    #         print("     attr:", attr)
-
-    # def handle_endtag(self, tag):
-    #     print("End tag  :", tag)
 
     def handle_data(self, data):
         if len(data) > 4:
-            if data.startswith("',") or data.endswith("'body_html': '"):
+            if data.startswith("', '") or data.endswith("'body_html': '"):
                 # print("Code     :" + data)
                 code_body.append("Code     :" + data)
             # else: 
                 # print("Data     :" + data)
 
-    # def handle_comment(self, data):
-    #     print("Comment  :", data)
-
-    # def handle_entityref(self, name):
-    #     c = chr(name2codepoint[name])
-    #     print("Named ent:", c)
-
-    # def handle_charref(self, name):
-    #     if name.startswith('x'):
-    #         c = chr(int(name[1:], 16))
-    #     else:
-    #         c = chr(int(name))
-    #     print("Num ent  :", c)
-
-    # def handle_decl(self, data):
-    #     print("Decl     :", data)
-
 parser = MyHTMLParser()
 unescaped = html.unescape(data_json)
 parsed = parser.feed(unescaped)
 
-columns = ["Author", "Author Fullname", "Name", "Created Time", "Parent ID", "Upvote", "Downvote"]
+columns = ["Author", "Author Fullname", "Name", "Parent ID", "Created Time", "Upvote", "Downvote"]
 rows = []
 
+@dataclass 
+class parsed_values:
+    author: str
+    author_fullname: str
+    name: str
+    parent_id: str
+    created_time: str
+    upvote: str
+    downvote: str
+
+def remove_extras(x):
+    '''
+    This function removes the extras character leftover from
+    the html parser.
+    
+    Args:
+        x (str): any string.
+    Returns:
+        str: corrected string.
+    '''
+    y = x.replace("'", "").replace("}}]", "").replace("}}", "").replace("\\n\\n", "").replace('\'', "'").replace(" \'", "'").replace(":\'", "").strip()
+    return y
+                                                                                          
+
 def code_parser(codes: list) -> list:
+    '''
+    This function plugs in the code_body list and parse them into
+    [[[a: ..],[b: ..],[c: ..],[d: ..]], [[a: ..],[b: ..],[c: ..],[d: ..]]]
+    
+    Args:
+        codes (str): list output from the html parser - code_body in this case.
+    Returns:
+        list: list of parsed value per person in the format above.
+    '''
+
+    raw = []
     rows = []
     for code in codes:
+        values = [remove_extras(code)]
+        raw.append(values)
+    for r in raw:
         row = []
-        for values in code:
-            value = values.strip().replace("'", "").replace("}}]", "").replace("}}", "").replace("\\n\\n", "").split(",")
-            row.append(value)
+        for values in r:
+            value = values.strip().split(",")
+            for v in value:
+                V = v.split(",")
+                row.append(V)
         rows.append(row)
     return rows
 
 def filtering_values(codes: list) -> list:
-    code_s = code_parser(codes)
+    '''
+    This function takes the value from code_parser(code_body) and filter them to 
+    the defined class parsed_values. If no value is found, "" will be used.
+    
+    Args:
+        codes (str): list output from the code_parser(code_body) in this case.
+    Returns:
+        list: list of class parsed_values for each person.
+    '''
+
+    one_person = []
     everyone = []
-    for code in code_s:
-        one_person = []
+
+    for code in codes[:-1]:
+        a_count = 0
+        a_f_count = 0
+        c_t_count = 0
+        u = ""
         for values in code:
             for value in values: 
-                if value.startswith("author:"):
-                    wanted_value = value.split(":")
-                    one_person.append(wanted_value[1].strip())
-                if value.startswith("name:"):
-                    wanted_value = value.split(":")
-                    one_person.append(wanted_value[1].strip())
-                if value.startswith("created_utc:"):
-                    wanted_value = value.split(":")
+                wanted_value = value.split(":")
+
+                if wanted_value[0] == " author" and a_count == 0:
+                    a = wanted_value[1].strip()
+                    a_count += 1
+
+                elif wanted_value[0] == " author_fullname" and a_f_count == 0:
+                    a_f = wanted_value[1].strip()
+                    a_count +=1
+
+                elif wanted_value[0] == " name":
+                    n = wanted_value[1].strip()
+
+                elif wanted_value[0] == " created_utc" and c_t_count == 0:
                     x = wanted_value[1].strip()
-                    converted_time = time.strftime("%D %H:%M", time.localtime(float(x)))
-                    one_person.append(converted_time)
-                if value.startswith("parent_id:"):
-                    wanted_value = value.split(":")
-                    one_person.append(wanted_value[1].strip())
-                if value.startswith("ups:"):
-                    wanted_value = value.split(":")
-                    one_person.append(wanted_value[1].strip())
-                if value.startswith("downs:"):
-                    wanted_value = value.split(":")
-                    one_person.append(wanted_value[1].strip())
+                    converted_time = str(time.strftime("%D %H:%M", time.localtime(float(x))))
+                    c_t_count += 1
+
+                elif wanted_value[0] == " parent_id":
+                    p_i = (wanted_value[1].strip())
+
+                elif wanted_value[0] == " ups":
+                    u = str(wanted_value[1].strip())
+
+                elif wanted_value[0] == " downs":
+                    d = str(wanted_value[1].strip())
+        
+        one_person = parsed_values(a, a_f, n, p_i, converted_time, u, d)
         everyone.append(one_person)
     return everyone
 
-print(filtering_values(code_body))
-# print(filtering_values(code_body))
-# print(type(code_parser(code_body)))
-# print(code_parser(code_body))
-# print(code_parser(code_body)[2])
+sorted_into_class = filtering_values(code_parser(code_body))
