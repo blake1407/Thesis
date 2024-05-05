4cbba5b5595bba9c8c0882adfa9423425bf73d8b
Ha Tran (Blake) <blaketrn@udel.edu>
2024-05-05 02:03:29 -0400

    some update

--- Influencer NER.py
+++ Influencer NER.py
@@ -11,9 +11,10 @@ from bs4 import BeautifulSoup
 from dataclasses import dataclass
 
 #Sentence Tokenization using sent_tokenize
-from nltk.tokenize import sent_tokenize
-import nltk.data
+import nltk
 # nltk.download('punkt')
+from nltk import word_tokenize,sent_tokenize
+
 
 #Detect language using detect_langs
 import langdetect
@@ -44,7 +45,7 @@ with open('log.csv', mode = 'r') as file:
     for number in all_no_comments:
         expected_no_comments += int(number)
 
-print(all_links)
+# print(all_links)
 #loop to open all post titles in create one big corpus of all comments
 def create_corpus(titles: list) -> str:
     """
@@ -64,7 +65,7 @@ def create_corpus(titles: list) -> str:
     empty = ""
     list_of_comments = []
     
-    base_folder = "Reddit Post Parsed"
+    base_folder = "Update posts files"
     for title in titles:
         title_csv = os.path.join(base_folder, title + "'s post.csv")
         if not os.path.isfile(title_csv):
@@ -78,7 +79,7 @@ def create_corpus(titles: list) -> str:
 
     for comment in list_of_comments:
         if comment.strip() != "Body":
-            if comment.strip() == '"[deleted]"' or comment.strip() == '"[removed]"':
+            if comment.strip() == '"deleted"' or comment.strip() == '"removed"':
                 no_deleted_comments +=1
                 comment = ""
             if comment.strip().startswith('"https:'):
@@ -87,7 +88,7 @@ def create_corpus(titles: list) -> str:
                 count_proper_comments += 1 
                 corpus = corpus + " " + comment.replace("**", "").replace("#", "").strip()[1:-1] 
     print(f'Number of comments yielded for the corpus (that are not urls or deleted): {count_proper_comments}.') 
-    print(f'Number of removed/deleted comments (has been filetered from corpus): {no_deleted_comments}.')                  
+    print(f'Number of removed/deleted comments (has been filetered from corpus): {no_deleted_comments}.\n')                  
                 
 create_corpus(all_post_titles)
 
@@ -103,7 +104,8 @@ pt_nlp = spacy.load('pt_core_news_sm')
 pd.set_option("display.max_rows", 200)
  
 #separate into tokenized sentences
-tokenizer = nltk.data.load('tokenizers/punkt/PY3/english.pickle')
+# tokenizer = nltk.data.load('tokenizers/punkt/PY3/english.pickle')
+tokenizer = nltk.data.load('tokenizers\punkt\english.pickle')
 sentences = tokenizer.tokenize(corpus)
 
 #Sentences with more than one languages detected
@@ -114,18 +116,22 @@ pt_s = []
 
 for sentence in sentences:
     #benchmark is with a language probablity over 0.5
-    score = detect_langs(sentence)
-    if len(score) > 1:
-        mixed_s.append(sentence)
-    for s in score:
-        if s.lang == 'en' and s.prob >= 0.5:
-            en_s.append(sentence)
-        if s.lang == 'es' and s.prob >= 0.5:
-            es_s.append(sentence)
-        if s.lang == 'pt' and s.prob >= 0.5:
-            pt_s.append(sentence)
-        else:
+    try:
+        score = detect_langs(sentence)
+        if len(score) > 1:
             mixed_s.append(sentence)
+        for s in score:
+            if s.lang == 'en' and s.prob >= 0.5:
+                en_s.append(sentence)
+            if s.lang == 'es' and s.prob >= 0.5:
+                es_s.append(sentence)
+            if s.lang == 'pt' and s.prob >= 0.5:
+                pt_s.append(sentence)
+            else:
+                mixed_s.append(sentence)
+    except:
+        # print("This throws an error: " + sentence)
+        continue 
 
 
 NER_output = []
@@ -139,50 +145,49 @@ class entities:
 en_doc = en_nlp(" ".join(en_s) + " ".join(mixed_s))
 pt_doc = pt_nlp(" ".join(es_s) + " ".join(pt_s))
 
-combined_doc = en_nlp(" ".join(en_s) + " ".join(mixed_s) + " ".join(es_s) + " ".join(pt_s))
-test_output = []
+combined_doc = en_nlp(corpus)
+test1_output = []
 
+#English NER model
 for ent in en_doc.ents:
-    # The output displayed the names of the entities and their predicted labels.
     if ent.text not in NER_output:
         NER_output.append(entities(ent.text, ent.label_))
+
+#Portuguese NER model
 for ent in pt_doc.ents:
-    # The output displayed the names of the entities and their predicted labels.
     if ent.text not in NER_output:
         NER_output.append(entities(ent.text, ent.label_))
 
+#English NER model
 for ent in combined_doc.ents:
-    # The output displayed the names of the entities and their predicted labels.
-    if ent.text not in test_output:
-        test_output.append(entities(ent.text, ent.label_)) 
-
-# print(len(test_output))
-# print(len(NER_output))
-
-
-
-# Find noun phrases in the corpus
-# print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
-
-# Find verbs in the corpus
-# print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
-
+    if ent.text not in test1_output:
+        test1_output.append(entities(ent.text, ent.label_)) 
 
 filtered_corpus = ""
+filtered1_corpus = ""
 def remove_output_from_corpus() -> str:
     #remove already found NER output from the general corpus.
     global NER_output
     global corpus
     global filtered_corpus
+    global filtered1_corpus
+
     filtered_corpus = corpus
     for entity in NER_output:
         filtered_corpus = filtered_corpus.replace(entity.name, "")
 
+    filtered1_corpus = corpus
+    for entity in test1_output:
+        filtered1_corpus = filtered1_corpus.replace(entity.name, "")
+
 remove_output_from_corpus()
 
 filtered_doc = en_nlp(filtered_corpus)
+filtered1_doc = en_nlp(filtered1_corpus)
+
 #find reminaing noun phrases
 remained_nouns = [chunk.text for chunk in filtered_doc.noun_chunks]
+remained1_nouns = [chunk.text for chunk in filtered1_doc.noun_chunks]
 
 ##strip out the pronouns, conjunctions, etc.!
 pronouns = ['I', 'you', 'my', 'mine', 'myself', 'we', 'us', 'our', 'ours', 'ourselves', 'you', 'you', 'your', 
@@ -192,19 +197,30 @@ pronouns = ['I', 'you', 'my', 'mine', 'myself', 'we', 'us', 'our', 'ours', 'ours
             "each", "either", "enough", "everybody", "everyone", "everything", "little", "much", "neither", "nobody", 
             "no one", "nothing", "one", "other", "somebody", "something", "Both", "few", "fewer", "many", "others", 
             "several", "All", "any", "more", "most", "someone", "none", "some", "such", "this", "that", "these", "those"
-            "who", "whom", "whose", "what", "which"
-]
-# print(remained_nouns)
+            "who", "whom", "whose", "what", "which", "those people", "these guys", "me", "some people"]
 
+#filter out pronouns from the remainded_nouns
+pronouns_lower = set([pronoun.lower() for pronoun in pronouns])
+
+# Filter out pronouns from the remained_nouns list
+filtered_remained_nouns = [noun for noun in remained_nouns if noun.lower() not in pronouns_lower]
+
+filtered1 = [noun for noun in remained1_nouns if noun.lower() not in pronouns_lower]
+
+
+# print(filtered_remained_nouns)
 # corpus_remained_nouns = "".join(remained_nouns)
 
-# print(remained_nouns)
+# Lula = ["Luiz Inácio Lula da Silva", "Luiz Inacio Lula da Silva", "da Silva", "President Jair", "leftist Luiz Inacio", "Luiz Inácio"]
+# Bolsonaro = ["Jair Bolsonaro", "Bolsonaro", "Lula"]
 
-Lula = ["Luiz Inácio Lula da Silva", "Luiz Inacio Lula da Silva", "da Silva", "President Jair", "leftist Luiz Inacio", "Luiz Inácio"]
-Bolsonaro = ["Jair Bolsonaro", "Bolsonaro", "Lula"]
+print("Method 1: Langdetect each sentence in the corpus and use 2 NER models (en & es)")
+print(f'Amount of NER output: {len(NER_output)}.')
+print(f'Amount of remained nouns: {len(filtered_remained_nouns)}.\n') 
 
-# def names_in_urls() -> list:
-#     global comment_urls
-#     for comment in comment_urls:
+print("Method 2: No langdetect and use 1 NER models (en) for the entire corpus")
+print(f'Amount of NER output: {len(test1_output)}.')
+print(f'Amount of remained nouns: {len(filtered1)}.\n') 
 
-# names_in_urls()
+for entity in test1_output:
+    print(entity)
